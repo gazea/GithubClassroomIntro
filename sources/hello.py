c603c93bdba79c452518686e89fd178f6557b88c
gazea <ghw205@exeter.ac.uk>
2021-12-10 13:56:06 +0000

    Update hello.py

--- sources/hello.py
+++ sources/hello.py
@@ -1,4 +1,8 @@
-# Define function called hello_world, with no arguments
-def hello_world():
-  # return output string
-    return "Hello!"
+# import required packages
+import numpy as np
+
+# function declaration
+def circumference(radius, dp=3):
+  # correct formula, make sure it only returns 3 dp
+    circumf = round(2*np.pi*radius, dp)
+    return circumf
