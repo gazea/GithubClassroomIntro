21a45b1596210cae44fa872e3185f6236dcff4d1
gazea <ghw205@exeter.ac.uk>
2021-12-10 14:00:15 +0000

    Update circle.py

--- sources/circle.py
+++ sources/circle.py
@@ -4,5 +4,5 @@ import numpy as np
 # function declaration
 def circumference(radius, dp=3):
   # correct formula, make sure it only returns 3 dp
-    circumf = round(2*np.pi*radius, dp)
-    return circumf
+  circumf = round(2*np.pi*radius, dp)
+  return circumf
